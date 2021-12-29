c346238891db73546b0370c3912e79f8ee8d1ecc
kotahorii <khorii.micron@gmail.com>
2021-12-29 16:38:34 +0900

    [Update] delete modal

--- src/components/organisms/modal/DeletePostModal.tsx
+++ src/components/organisms/modal/DeletePostModal.tsx
@@ -1,32 +1,20 @@
-import { XIcon, TrashIcon } from '@heroicons/react/outline'
+import { TrashIcon } from '@heroicons/react/outline'
 import { useMain } from 'hooks/useMain'
 import { memo } from 'react'
 
 export const DeletePostModal = memo(() => {
-  const { detailUserPost, deletePost, closeDeletePostModal } = useMain()
+  const { detailUserPost, deletePost } = useMain()
 
   return (
-    <div className="flex flex-col mt-5 space-y-4 md:w-80 w-72">
+    <div className="flex flex-row items-center justify-between">
       <p>本当に削除しますか?</p>
-      <div className="flex flex-row justify-between">
-        <div></div>
-        <div className="flex flex-row">
-          <div
-            onClick={closeDeletePostModal}
-            className="flex flex-row items-center hover:bg-gray-200 p-2 rounded-lg cursor-pointer"
-          >
-            <XIcon className="w-5" />
-            <p>キャンセル</p>
-          </div>
-          <div
-            onClick={deletePost(detailUserPost.id)}
-            className="flex flex-row items-center hover:bg-gray-200 p-2 rounded-lg cursor-pointer"
-          >
-            <TrashIcon className="w-5" />
-            <p>削除</p>
-          </div>
-        </div>
-      </div>
+      <button
+        onClick={deletePost(detailUserPost.id)}
+        className="flex flex-row justify-center items-center space-x-1 bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-full"
+      >
+        <TrashIcon className="w-5" />
+        <p>削除</p>
+      </button>
     </div>
   )
 })
